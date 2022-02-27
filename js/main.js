276905ef62315297a24aa02e4269d428ff79f5ef
SergeyZasimov <zasimov.ser@yandex.ru>
2022-02-27 12:31:42 +0300

    Определение констант

--- js/main.js
+++ js/main.js
@@ -1,4 +1,20 @@
 const SIMILAR_ADVERTISEMENTS = 10;
+const LOW_PRICE = 1;
+const HIGH_PRICE = 1000;
+const TYPES = ['palace', 'flat', 'house', 'bungalow', 'hotel'];
+const CHECKIN_TIMES = ['12:00', '13:00', '14:00'];
+const CHECKOUT_TIMES = ['12:00', '13:00', '14:00'];
+const FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
+const PHOTO_PATHS = [
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg',
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg',
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg'
+];
+const LOW_LAT = 35.65000;
+const HI_LAT = 35.7000;
+const LOW_LNG = 139.70000;
+const HI_LNG = 139.80000;
+const DIGITS = 5;
 
 const getRandomNumber = (begin, end, digits = 0) => {
 
