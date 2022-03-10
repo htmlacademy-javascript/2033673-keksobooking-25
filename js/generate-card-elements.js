a3bac85d52405a0453e0f58a868567688223512c
SergeyZasimov <zasimov.ser@yandex.ru>
2022-03-10 11:19:27 +0300

    Вывод доступных удобств

--- js/generate-card-elements.js
+++ js/generate-card-elements.js
@@ -6,6 +6,18 @@ const addTextContent = (parent, selector, text) => {
   parent.querySelector(selector).textContent = text;
 };
 
+const addFeatures = (cardElement, features) => {
+  const featuresElementsList = cardElement.querySelectorAll('.popup__feature');
+
+  featuresElementsList.forEach((element) => {
+    const isAvailable = features.some((feature) => element.classList.contains(`popup__feature--${ feature }`));
+
+    if (!isAvailable) {
+      element.remove();
+    }
+  });
+};
+
 const generateCardElements = (cards) => {
   const fragment = document.createDocumentFragment();
   const template = document.querySelector('#card').content.querySelector('.popup');
@@ -13,16 +25,18 @@ const generateCardElements = (cards) => {
                    avatar,
                    offer: {title, address, price, type, rooms, guests, checkin, checkout, features, description, photos}
                  }) => {
-    const cardHTML = template.cloneNode(true);
-    addTextContent(cardHTML, '.popup__title', title);
-    addTextContent(cardHTML, '.popup__text--address', address);
-    addTextContent(cardHTML, '.popup__text--price', `${ price } \u{20BD}/ночь`);
-    addTextContent(cardHTML, '.popup__type', `${ TYPES[type] }`);
-    addTextContent(cardHTML, '.popup__text--capacity', `${ rooms } комнаты для ${ guests } гостей`);
-    addTextContent(cardHTML, '.popup__text--time', `Заезд после ${checkin}, выезд до ${checkout}`)
+    const cardElement = template.cloneNode(true);
+    addTextContent(cardElement, '.popup__title', title);
+    addTextContent(cardElement, '.popup__text--address', address);
+    addTextContent(cardElement, '.popup__text--price', `${ price } \u{20BD}/ночь`);
+    addTextContent(cardElement, '.popup__type', `${ TYPES[type] }`);
+    addTextContent(cardElement, '.popup__text--capacity', `${ rooms } комнаты для ${ guests } гостей`);
+    addTextContent(cardElement, '.popup__text--time', `Заезд после ${ checkin }, выезд до ${ checkout }`);
+    addTextContent(cardElement, '.popup__description', description);
+    addFeatures(cardElement, features);
 
 
-    fragment.append(cardHTML);
+    fragment.append(cardElement);
   });
   document.querySelector('#map-canvas').append(fragment.children[0]);
 };
