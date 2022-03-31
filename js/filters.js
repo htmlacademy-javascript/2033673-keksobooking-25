014d8bb06ac4e19cc9c38b5852463b45dbd38f39
SergeyZasimov <zasimov.ser@yandex.ru>
2022-03-31 19:25:04 +0300

    Фильтрация по цене, комнатам и гостям

--- js/filters.js
+++ js/filters.js
@@ -1,13 +1,49 @@
-import { getFilters } from './elements.js';
+import { getElements } from './elements.js';
 
-const { typeFilter } = getFilters();
+const { mapFilters } = getElements();
+
+const typeFilter = (item, type) => {
+  return item.offer.type === type || type === 'any';
+};
+
+const priceFilter = (item, priceLevel) => {
+  switch (priceLevel) {
+    case 'any':
+      return true;
+    case 'middle':
+      return item.offer.price >= 10000 && item.offer.price <= 50000;
+    case 'low':
+      return item.offer.price < 10000;
+    case 'high':
+      return item.offer.price > 50000;
+  }
+};
+
+const roomsFilter = (item, rooms) => {
+  return item.offer.rooms === +rooms || rooms === 'any';
+};
+
+const guestsFilter = (item, guests) => {
+  return item.offer.guests === +guests || guests === 'any';
+};
 
 const setFilters = (advertisements) => {
-  let filterAdvertisements;
-  typeFilter.addEventListener('change', (e) => {
-    filterAdvertisements = advertisements.filter((ad) => ad.offer.type === e.target.value);
+  const filters = {
+    type: 'any',
+    price: 'any',
+    rooms: 'any',
+    guests: 'any',
+  };
+  mapFilters.addEventListener('change', (e) => {
+    filters[e.target.id.split('-')[1]] = e.target.value;
+    const filterAdvertisements = advertisements
+      .filter((item) => typeFilter(item, filters.type))
+      .filter((item) => priceFilter(item, filters.price))
+      .filter((item) => roomsFilter(item, filters.rooms))
+      .filter((item) => guestsFilter(item, filters.guests));
     console.log(filterAdvertisements);
   });
 };
+
 
 export { setFilters };
